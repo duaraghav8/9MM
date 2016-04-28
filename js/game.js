b7382e7ff877dbd7f620b85f07218a083f1340ca
duaraghav8@gmail <duaraghav8@gmail.com>
2016-04-28 19:19:45 +0530

    refactored for OOP and player & tile positioning

--- js/game.js
+++ js/game.js
@@ -1,44 +1,47 @@
-var Board = function () {};
 var Tile = function (attr) {
   this.attributes = attr;   //attr = {id: .., position: [], status: {occupied: .., player: ..}, }
 };
+
 var Player = function (attr) {
   this.attributes = attr;   //attr = {id: .., position: [], status: {alive: .., occupies: .., tile: ..}, }
 };
-var Game = function () {};
 
-var tiles = [], players = [];
+var Game = function () {
+  this.tiles = [];
+  this.players = [];
+  var gameScope = this;
 
-function init () {
-  for (var id = 0; id < 24; id++) {
-    var tileAttr = {
-      id: id,
-      position: {x: undefined, y: undefined},
-      status: {
-        occupied: false,
-        player: undefined
-      },
-      neighbours: {left: undefined, right: undefined, upper: undefined, lower: undefined}
-    };
+  (function init () {
+    for (var id = 0; id < 24; id++) {
+      var tileAttr = {
+        id: id,
+        position: {x: undefined, y: undefined},
+        status: {
+          occupied: false,
+          player: undefined
+        },
+        neighbours: {left: undefined, right: undefined, upper: undefined, lower: undefined}
+      };
 
-    tiles.push (tileAttr);
-  }
+      gameScope.tiles.push (tileAttr);
+    }
 
-  var currentTeam = 1;
-  for (var i = 0; i < 18; i++) {
-    if (i === 9) { currentTeam = Number (!currentTeam); }
+    var currentTeam = 1;
+    for (var i = 0; i < 18; i++) {
+      if (i === 9) { currentTeam = Number (!currentTeam); }
 
-    players.push ({
-      id: i % 9,
-      position: {},    //based on whether its Team 1 / 0 and other factors
-      status: {
-        alive: true,
-        occupies: false,
-        tile: null
-      },
-      team: currentTeam
-    });
-  }
+      gameScope.players.push ({
+        id: i % 9,
+        position: {x: undefined, y: undefined},    //based on whether its Team 1 / 0 and other factors
+        status: {
+          alive: true,
+          occupies: false,
+          tile: null
+        },
+        team: currentTeam
+      });
+    }
+  }) ();
 };
 
-init ();
+var game = new Game ();
