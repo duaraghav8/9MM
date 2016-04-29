ac6de29adb6b91393aef21863f54be36930af73e
Raghav Dua <duaraghav8@gmail.com>
2016-04-29 11:08:53 +0530

    Working on Game API

--- js/game.js
+++ js/game.js
@@ -9,6 +9,7 @@ var Player = function (attr) {
 var Game = function () {
   this.tiles = [];
   this.players = [];
+  this.phase1Counter = 0;
   var gameScope = this;
 
   (function init () {
@@ -28,11 +29,9 @@ var Game = function () {
 
     var currentTeam = 1;
     for (var i = 0; i < 18; i++) {
-      if (i === 9) { currentTeam = Number (!currentTeam); }
-
       gameScope.players.push ({
-        id: i % 9,
-        position: {x: undefined, y: undefined},    //based on whether its Team 1 / 0 and other factors
+        id: i,
+        position: {x: undefined, y: undefined},    //based on whether its Team 1(W) / 0(B) and other factors
         status: {
           alive: true,
           occupies: false,
@@ -40,8 +39,54 @@ var Game = function () {
         },
         team: currentTeam
       });
+      currentTeam = Number (!currentTeam);
     }
   }) ();
 };
 
-var game = new Game ();
+Game.prototype = {
+  getTiles: function () { return (this.tiles); },
+  getPlayers: function () { return (this.players); },
+
+  selectPlayer: function (player) {
+    console.log ('selecting player', this.players [player.id]);
+    /*
+      if not phase 1:
+        select this player, return true
+      else don't select player & return false
+
+      if there was another player previously selected, return its id so we can de-select it visually (change color)
+    */
+    return new Promise (function (resolve, reject) {
+      if (true) {
+        resolve ({succeeded: true, previouslySelectPlayer: undefined});
+      }
+      else {
+        reject ();
+      }
+    });
+  },
+  moveSelectedPlayer: function (tile) {
+    console.log ('moving to--->', tile, this.tiles [tile.id]);
+
+    return new Promise (function (resolve, reject) {
+      /*
+        if phase 1: currently selected player is automatic, else manual
+        if phase 2: check if the tile selected is valid for the selected player to move, i.e., a neighbouring tile
+      */
+      if (true) {
+        //change player properties before sending
+        resolve ({succeeded: true, currentlySelectedPlayer: undefined});   //return currently selected player
+      }
+      else {
+        reject ();
+      }
+    });
+  },
+
+  phase1: function () {
+    this.phase1Counter > 17 ? this.phase2 () : null;
+  },
+  phase2: function () {},
+  phase3: function () {}
+};
